75e266340a5615cd8f628fee3d00e5f21107fc50
Vadim152 <139229559+Vadim152@users.noreply.github.com>
2025-06-09 17:58:59 +0300

    Add mobile-style camera controller

---
+++ Assets/Scripts/CameraTapController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraTapController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Camera cam;
+    private Vector3 targetPos;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        Screen.orientation = ScreenOrientation.Portrait;
+        cam.aspect = 9f / 16f;
+        targetPos = cam.transform.position;
+    }
+
+    void Update()
+    {
+        bool tapped = false;
+        Vector3 screenPos = Vector3.zero;
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            tapped = true;
+            screenPos = Input.GetTouch(0).position;
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            tapped = true;
+            screenPos = Input.mousePosition;
+        }
+
+        if (tapped)
+        {
+            Vector3 world = cam.ScreenToWorldPoint(screenPos);
+            world.z = targetPos.z;
+            targetPos = world;
+        }
+
+        cam.transform.position = Vector3.Lerp(cam.transform.position, targetPos, moveSpeed * Time.deltaTime);
+    }
+}
